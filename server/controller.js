a8dc1d1aa74ef420783bcf4185a214c1b64a1209
Paul Okoh <pauldokoh@gmail.com>
2022-12-02 16:03:01 -0600

    add random fortunes feature

--- server/controller.js
+++ server/controller.js
@@ -8,6 +8,24 @@ module.exports = {
         let randomCompliment = compliments[randomIndex];
       
         res.status(200).send(randomCompliment);
+    },
+
+    getFortune: (req, res) => {
+        const fortunes = ["If you look in the right places, you can find some good offerings",
+            "If you think you can do a thing or think you can't do a thing, you're right",
+            "If you wish to see the best in others, show the best of yourself",
+            "If your desires are not extravagant, they will be granted",
+            "If you're feeling down, try throwing yourself into your work",
+            "Imagination rules the world",
+            "In order to take, one must first give"]; 
+
+            let randomFortuneIndex = Math.floor(Math.random() * fortunes.length);
+        let randomFortune = fortunes[randomFortuneIndex];
+      
+        res.status(200).send(randomFortune);
+
+
     }
+
 
 }
